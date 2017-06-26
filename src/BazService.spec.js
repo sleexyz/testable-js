261030c6cc63016fcdb41624f77d1ce775ada9ae
Sean Lee <freshdried@gmail.com>
2017-06-26 00:53:45 -0400

    scala style testing

--- src/BazService.spec.js
+++ src/BazService.spec.js
@@ -10,7 +10,7 @@ import type { BazService } from './BazService';
 
 describe('BazService', () => {
   describe('method', () => {
-    class Test {
+    class BazServiceTest {
       fooService: FooService
       barService: BarService
       bazService: BazService
@@ -28,19 +28,19 @@ describe('BazService', () => {
         this.bazService = env.bazService;
       }
     }
-    it('appends text properly', () => {
-      const ctx = new Test();
-      expect(ctx.bazService.method()).to.eql('baz: 2 bar-test');
-    });
-    it('calls fooService.method2 once', () => {
-      const ctx = new Test();
-      ctx.bazService.method();
-      expect(ctx.fooService.method2.callCount).to.eql(1);
-    });
-    it('calls barService.method once', () => {
-      const ctx = new Test();
-      ctx.bazService.method();
-      expect(ctx.barService.method.callCount).to.eql(1);
-    });
+
+    it('appends text properly', function () {
+      expect(this.bazService.method()).to.eql('baz: 2 bar-test');
+    }.bind(new BazServiceTest()));
+
+    it('calls fooService.method2 once', function () {
+      this.bazService.method();
+      expect(this.fooService.method2.callCount).to.eql(1);
+    }.bind(new BazServiceTest()));
+
+    it('calls barService.method once', function () {
+      this.bazService.method();
+      expect(this.barService.method.callCount).to.eql(1);
+    }.bind(new BazServiceTest()));
   });
 });
